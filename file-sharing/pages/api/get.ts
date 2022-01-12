8a39cd74505be71b77b6d79cf511ead2e5aa809f
Emin Deniz <emindeniz99@gmail.com>
2022-01-12 09:50:16 +0000

    prettier and fix lint issues

--- file-sharing/pages/api/get.ts
+++ file-sharing/pages/api/get.ts
@@ -1,77 +1,77 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next"
-import nextConnect from "next-connect"
-import multer, { memoryStorage } from "multer"
-import multerS3 from "multer-s3"
-import aws from "aws-sdk"
-import crypto from "crypto"
-import bodyParser from "body-parser"
+import type { NextApiRequest, NextApiResponse } from "next";
+import nextConnect from "next-connect";
+import multer, { memoryStorage } from "multer";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
+import crypto from "crypto";
+import bodyParser from "body-parser";
 var s3 = new aws.S3({
-	/* ... */
-	accessKeyId: process.env.accessKeyId,
-	secretAccessKey: process.env.secretAccessKey,
-})
+  /* ... */
+  accessKeyId: process.env.accessKeyId,
+  secretAccessKey: process.env.secretAccessKey,
+});
 
 const upload = multer({
-	storage: memoryStorage(),
+  storage: memoryStorage(),
 
-	// multer.diskStorage({
-	// 	destination: "./public/uploads",
-	// 	filename: (req, file, cb) => cb(null, file.originalname),
-	// }),
-})
+  // multer.diskStorage({
+  // 	destination: "./public/uploads",
+  // 	filename: (req, file, cb) => cb(null, file.originalname),
+  // }),
+});
 
 const apiRoute = nextConnect<NextApiRequest, NextApiResponse>({
-	onError(error, req, res) {
-		console.error(error)
+  onError(error, req, res) {
+    console.error(error);
 
-		res
-			.status(501)
-			.json({ error: `Sorry something Happened! ${error.message}` })
-	},
-	onNoMatch(req, res) {
-		res.status(405).json({ error: `Method '${req.method}' Not Allowed` })
-	},
-})
+    res
+      .status(501)
+      .json({ error: `Sorry something Happened! ${error.message}` });
+  },
+  onNoMatch(req, res) {
+    res.status(405).json({ error: `Method '${req.method}' Not Allowed` });
+  },
+});
 
 // apiRoute.use()
 
 apiRoute.get(bodyParser.json(), async (req, res) => {
-	console.log(Date.now())
-	console.log(req.query.id)
+  console.log(Date.now());
+  console.log(req.query.id);
 
-	try {
-		const objHead = await s3
-			.headObject({
-				Bucket: "cmpe48a-project",
-				Key: req.query.id as string,
-			})
-			.promise()
-		console.log(objHead)
+  try {
+    const objHead = await s3
+      .headObject({
+        Bucket: "cmpe48a-project",
+        Key: req.query.id as string,
+      })
+      .promise();
+    console.log(objHead);
 
-		const filename = objHead.Metadata?.originalname
-		const contentType = objHead.ContentType as string
-		s3.getObject({
-			Bucket: "cmpe48a-project",
-			Key: req.query.id as string,
-		})
-			.createReadStream()
-			// https://stackoverflow.com/questions/35386749/how-download-a-file-from-amazon-s3-with-express-js-and-request-js/35386796#35386796
-			.pipe(
-				res
-					.setHeader("Content-Type", contentType)
-					.setHeader("Content-Disposition", `inline; filename="${filename}"`)
-			)
-	} catch (error) {
-		console.error(error)
-		res.status(500).json({ data: "error", error })
-	}
-})
+    const filename = objHead.Metadata?.originalname;
+    const contentType = objHead.ContentType as string;
+    s3.getObject({
+      Bucket: "cmpe48a-project",
+      Key: req.query.id as string,
+    })
+      .createReadStream()
+      // https://stackoverflow.com/questions/35386749/how-download-a-file-from-amazon-s3-with-express-js-and-request-js/35386796#35386796
+      .pipe(
+        res
+          .setHeader("Content-Type", contentType)
+          .setHeader("Content-Disposition", `inline; filename="${filename}"`)
+      );
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ data: "error", error });
+  }
+});
 
-export default apiRoute
+export default apiRoute;
 
 export const config = {
-	api: {
-		bodyParser: false, // Disallow body parsing, consume as stream
-	},
-}
+  api: {
+    bodyParser: false, // Disallow body parsing, consume as stream
+  },
+};
--- file-sharing/pages/api/upload.ts
+++ file-sharing/pages/api/upload.ts
@@ -1,76 +1,77 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next"
-import nextConnect from "next-connect"
-import multer, { memoryStorage } from "multer"
-import multerS3 from "multer-s3"
-import aws from "aws-sdk"
-import crypto from "crypto"
+import type { NextApiRequest, NextApiResponse } from "next";
+import nextConnect from "next-connect";
+import multer, { memoryStorage } from "multer";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
+import crypto from "crypto";
 var s3 = new aws.S3({
-	/* ... */
-	accessKeyId: process.env.accessKeyId,
-	secretAccessKey: process.env.secretAccessKey,
-})
+  /* ... */
+  accessKeyId: process.env.accessKeyId,
+  secretAccessKey: process.env.secretAccessKey,
+});
 
 const upload = multer({
-	storage: memoryStorage(),
+  storage: memoryStorage(),
 
-	// multer.diskStorage({
-	// 	destination: "./public/uploads",
-	// 	filename: (req, file, cb) => cb(null, file.originalname),
-	// }),
-})
+  // multer.diskStorage({
+  // 	destination: "./public/uploads",
+  // 	filename: (req, file, cb) => cb(null, file.originalname),
+  // }),
+});
 
 const apiRoute = nextConnect<NextApiRequest, NextApiResponse>({
-	onError(error, req, res) {
-		console.error(error)
+  onError(error, req, res) {
+    console.error(error);
 
-		res
-			.status(501)
-			.json({ error: `Sorry something Happened! ${error.message}` })
-	},
-	onNoMatch(req, res) {
-		res.status(405).json({ error: `Method '${req.method}' Not Allowed` })
-	},
-})
+    res
+      .status(501)
+      .json({ error: `Sorry something Happened! ${error.message}` });
+  },
+  onNoMatch(req, res) {
+    res.status(405).json({ error: `Method '${req.method}' Not Allowed` });
+  },
+});
 
 // apiRoute.use()
 
 apiRoute.post(upload.array("theFiles"), async (req, res) => {
-	console.log(Date.now())
-	console.log(req.files)
-	if (req.files?.length == 0)
-		return res.json({ error: "error", files: req.files })
-	const hashSum = crypto.createHash("sha256")
-	hashSum.update(req.files[0].buffer)
+  console.log(Date.now());
+  console.log(req.files);
+  if (!req.files || !(req.files && (req.files as any)[0]))
+    return res.json({ error: "error", files: req.files });
+  const hashSum = crypto.createHash("sha256");
+  const file: Express.Multer.File = (req.files as any)[0];
 
-	const hex = hashSum.digest("hex")
-	console.log("🚀 ~ file: hello.ts ~ line 43 ~ apiRoute.post ~ hex", hex)
-	try {
-		const file: Express.Multer.File = req.files[0]
-		console.log(file.mimetype)
+  hashSum.update(file.buffer);
 
-		const result = await s3
-			.upload({
-				Bucket: "cmpe48a-project",
-				Key: hex, // File name you want to save as in S3
-				Body: file.buffer,
-				ContentType: file.mimetype,
-				Metadata: {
-					originalname: file.originalname,
-				},
-			})
-			.promise()
-		res.status(200).json({ data: "success", result })
-	} catch (error) {
-		console.error(error)
-		res.status(500).json({ data: "error", error })
-	}
-})
+  const hex = hashSum.digest("hex");
+  console.log("🚀 ~ file: hello.ts ~ line 43 ~ apiRoute.post ~ hex", hex);
+  try {
+    console.log(file.mimetype);
 
-export default apiRoute
+    const result = await s3
+      .upload({
+        Bucket: "cmpe48a-project",
+        Key: hex, // File name you want to save as in S3
+        Body: file.buffer,
+        ContentType: file.mimetype,
+        Metadata: {
+          originalname: file.originalname,
+        },
+      })
+      .promise();
+    res.status(200).json({ data: "success", result });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ data: "error", error });
+  }
+});
+
+export default apiRoute;
 
 export const config = {
-	api: {
-		bodyParser: false, // Disallow body parsing, consume as stream
-	},
-}
+  api: {
+    bodyParser: false, // Disallow body parsing, consume as stream
+  },
+};
